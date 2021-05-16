1afd02bfe1845cb3860c1be8192a13bc5fc7a48e
jennygerstle@gmail.com <jennygerstle@gmail.com>
2021-05-16 15:05:41 -0400

    got rid of interceptors

--- src/main/java/gerstle/stock/LatestStockService.java
+++ src/main/java/gerstle/stock/LatestStockService.java
@@ -2,13 +2,15 @@ package gerstle.stock;
 
 import io.reactivex.rxjava3.core.Single;
 import retrofit2.http.GET;
-import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.Query;
 
 import java.net.http.HttpResponse;
 
 public interface LatestStockService
 {
+    @Headers("x-rapidapi-key", "3d3bec4cf6msh5e70c5761ec73d4p1df9e5jsne9a9941276b6")
+    @Headers("x-rapidapi-host", "latest-stock-price.p.rapidapi.com")
     @GET("3d3bec4cf6msh5e70c5761ec73d4p1df9e5jsne9a9941276b6")
     Single<LatestStockFeed> getStockPrice();
 }
--- src/main/java/gerstle/stock/LatestStockServiceFactory.java
+++ src/main/java/gerstle/stock/LatestStockServiceFactory.java
@@ -16,27 +16,8 @@ import java.net.http.HttpResponse;
 
 public class LatestStockServiceFactory
 {
-    public LatestStockService newInstance() throws IOException, InterruptedException
+    public LatestStockService newInstance()
     {
-        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-        httpClient.addInterceptor(new Interceptor()
-                                  {
-                                      @Override
-                                      public Response intercept(Interceptor.Chain chain) throws IOException
-                                      {
-                                          Request original = chain.request();
-
-                                          Request request = original.newBuilder()
-                                                  .header("x-rapidapi-key", "3d3bec4cf6msh5e70c5761ec73d4p1df9e5jsne9a9941276b6")
-                                                  .header("x-rapidapi-host", "latest-stock-price.p.rapidapi.com")
-                                                  .method(original.method(), original.body())
-                                                  .build();
-
-                                          return chain.proceed(request);
-                                      }
-                                  }    );
-
-                OkHttpClient client = httpClient.build();
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://rapidapi.com/suneetk92/api/latest-stock-price/")
                 .addConverterFactory(GsonConverterFactory.create())
